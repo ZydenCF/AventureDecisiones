93c1d6ca329e4c178fb9a5ea175f78b29ac6ed66
David Ccolcca <draigner143@gmail.com>
2025-09-03 17:13:36 -0500

    Create IInteractuable.cs

---
+++ Aventura Decisiones/IInteractuable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aventura_Decisiones
+{
+    public interface IInteractuable
+    {
+        string Interactuar(Jugador jugador);
+    }
+}
