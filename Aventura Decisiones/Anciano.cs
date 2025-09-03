683de94434aaeed93879d2645e0ffca1b9c5bb1b
David Ccolcca <draigner143@gmail.com>
2025-09-03 17:09:17 -0500

    Create Anciano.cs

---
+++ Aventura Decisiones/Anciano.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aventura_Decisiones
+{
+    public class Anciano : Personaje
+    {
+        public string Mensaje;
+        public Anciano(string nombre) : base(nombre)
+        {
+            this.Mensaje = "La naturaleza siempre responde a quien escucha.";
+        }
+
+        public override string Hablar()
+        {
+            return this.Nombre + this.Mensaje;
+        }
+    }
+}
